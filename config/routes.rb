63e3c93ff02968218f2da96545331409637f67d9
Arielle Gruman <arielle.gruman@yahoo.com>
2015-08-01 14:26:34 -0400

    start sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resource :users
 
+  root to: 'sessions#new'
+  resource :sessions
+  resource :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
