88c2d1f4fc2dcc8e34c2d67094a910522a3345bf
Arielle Gruman <arielle.gruman@yahoo.com>
2015-08-05 17:33:17 -0400

    put chats on book show page

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,7 @@
 class Book < ActiveRecord::Base
 
   belongs_to :user
-
+  has_many :chats
   has_many :user_books
   has_many :users, through: :user_books
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
 
+  has_many :user_books
   has_many :chats
   has_many :books, through: :user_books
 end
