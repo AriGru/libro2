ddc01d762412295adb9b362fe3d2143bce4daf66
Arielle Gruman <arielle.gruman@yahoo.com>
2015-08-04 20:45:56 -0400

    updated user and book table

---
+++ db/migrate/20150804232754_remove_book_from_users.rb
@@ -0,0 +1,5 @@
+# class RemoveBookFromUsers < ActiveRecord::Migration
+#   def change
+#     remove_reference :users, :book, index: true, foreign_key: true
+#   end
+# end
--- db/schema.rb
+++ db/schema.rb
@@ -11,21 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150804215009) do
+ActiveRecord::Schema.define(version: 20150804232729) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "books", force: :cascade do |t|
-    t.integer  "user_id"
     t.string   "title"
     t.string   "author"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
-
-  add_index "books", ["user_id"], name: "index_books_on_user_id", using: :btree
 
   create_table "chats", force: :cascade do |t|
     t.text     "comment"
@@ -56,7 +53,6 @@ ActiveRecord::Schema.define(version: 20150804215009) do
 
   add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
-  add_foreign_key "books", "users"
   add_foreign_key "chats", "books"
   add_foreign_key "user_books", "books"
   add_foreign_key "user_books", "users"
