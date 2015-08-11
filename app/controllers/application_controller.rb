5140ef7370103c957dacba7ebdabdb4c12487825
Sofia Garcia <sofisagarcia@gmail.com>
2015-08-11 18:01:58 -0400

    initialize search functionality-- elasticsearch and searchkick

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  def index
+        @books = Book.search(params[:search])
+  end
 
   include PublicActivity::StoreController
 
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,6 +7,7 @@ class BooksController < ApplicationController
   # GET /books.json
   def index
     @books = Book.all
+    @books = Book.search(params[:search])
   end
 
   # GET /books/1
