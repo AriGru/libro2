177933b214f143647ca996f331be6b3a0f126289
Sofia Garcia <sofisagarcia@gmail.com>
2015-08-10 23:14:28 -0400

    header/applications.html.erb will not show if current_user is nil -- profile will not link if there is no user

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
   tracked owner: ->(controller, model) { controller && controller.current_user }
 
   has_secure_password
-
+  
   has_many :user_books
   has_many :chats
   has_many :books, through: :user_books
--- app/models/user_book.rb
+++ app/models/user_book.rb
@@ -1,4 +1,5 @@
 class UserBook < ActiveRecord::Base
+  include PublicActivity::Model
   belongs_to :user
   belongs_to :book
 end
