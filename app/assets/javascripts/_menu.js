ec3a1cb5793faad69991e78f8d55d8d900fedd08
Sofia Garcia <sofisagarcia@gmail.com>
2015-08-11 10:28:09 -0400

    turbolinks/js hamburger menu working

--- app/assets/javascripts/_menu.js
+++ app/assets/javascripts/_menu.js
@@ -1,6 +1,7 @@
 
-$(document).ready(function(){
-	$('.hamburger-shell').click(function(){
+function ready() {
+  console.log('hello');
+  $('.hamburger-shell').click(function(){
 		$('#menu').slideToggle(300);
 		$('.top').toggleClass('rotate');
 		$('.middle').toggleClass('rotate-back');
@@ -13,5 +14,25 @@ $(document).ready(function(){
 		$('.middle').toggleClass('rotate-back');
 		$('.menu-name').toggleClass('bump');
 		// $('.bg-cover').toggleClass('reveal');
-	})
-});
+	});
+};
+$(document).ready(ready);
+$(document).on('page:load', ready);
+
+
+// $(document).ready(function(){
+// 	$('.hamburger-shell').click(function(){
+// 		$('#menu').slideToggle(300);
+// 		$('.top').toggleClass('rotate');
+// 		$('.middle').toggleClass('rotate-back');
+// 		$('.menu-name').toggleClass('bump');
+// 		// $('.bg-cover').toggleClass('reveal');
+// 	});
+// 	$('.bg-cover').click(function(){
+// 		$('#menu').slideToggle(300);
+// 		$('.top').toggleClass('rotate');
+// 		$('.middle').toggleClass('rotate-back');
+// 		$('.menu-name').toggleClass('bump');
+// 		// $('.bg-cover').toggleClass('reveal');
+// 	})
+// });
